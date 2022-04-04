8e49746c26b0e8e5fec1887bcfd353e348f273fc
fang duan <df1228@gmail.com>
2022-04-04 22:29:23 +0800

    Fix duplicate key error

--- rpc/internal/logic/shorten_logic.go
+++ rpc/internal/logic/shorten_logic.go
@@ -2,6 +2,7 @@ package logic
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/dfang/shorturl/rpc/internal/svc"
 	"github.com/dfang/shorturl/rpc/model"
@@ -27,13 +28,26 @@ func NewShortenLogic(ctx context.Context, svcCtx *svc.ServiceContext) *ShortenLo
 
 func (l *ShortenLogic) Shorten(in *transform.ShortenRequest) (*transform.ShortenResponse, error) {
 	key := hash.Md5Hex([]byte(in.Url))[:6]
-	_, err := l.svcCtx.Model.Insert(l.ctx, &model.Shorturl{
+
+	// 数据库有这条记录，不用插入了
+	shorturl, err := l.svcCtx.Model.FindOne(l.ctx, key)
+	if shorturl != nil && err == nil {
+		fmt.Println("数据库有这条记录，不用插入了")
+		return &transform.ShortenResponse{
+			Shorten: key,
+		}, nil
+	}
+
+	result, err := l.svcCtx.Model.Insert(l.ctx, &model.Shorturl{
 		Shorten: key,
 		Url:     in.Url,
 	})
 	if err != nil {
 		return nil, err
 	}
+
+	id, _ := result.LastInsertId()
+	logx.Info("last insert id: ", id)
 
 	return &transform.ShortenResponse{
 		Shorten: key,
